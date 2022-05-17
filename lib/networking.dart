2d5e4a7ef14e4fe2cd694ffbdc37facc0d7a392d
freitzzz <1160907@isep.ipp.pt>
2022-05-17 18:30:58 +0100

    export networking.dart on top level barrel

--- lib/networking.dart
+++ lib/networking.dart
@@ -3,6 +3,4 @@
 /// More dartdocs go here.
 library networking;
 
-export 'src/networking_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/networking.dart';
