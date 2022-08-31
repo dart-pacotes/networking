877507e8d295c6e38d194e4b865d1b82275fdbc7
freitzzz <1160907@isep.ipp.pt>
2022-08-31 14:35:13 +0100

    include more content types

--- lib/src/http/models/content_type.dart
+++ lib/src/http/models/content_type.dart
@@ -1,44 +1,87 @@
+// TODO: In Dart 3 we can use enum constructors, which should remove the need for these maps
+
+const _kContentTypeForValue = {
+  ContentType.binary: 'application/octet-stream',
+  ContentType.formData: 'multipart/form-data',
+  ContentType.jpeg: 'image/jpeg',
+  ContentType.json: 'application/json',
+  ContentType.png: 'image/png',
+  ContentType.avif: 'image/avif',
+  ContentType.bmp: 'image/bmp',
+  ContentType.gif: 'image/gif',
+  ContentType.gzip: 'application/gzip',
+  ContentType.ico: 'image/vnd.microsoft.icon',
+  ContentType.jsonld: 'application/ld+json',
+  ContentType.pdf: 'application/pdf',
+  ContentType.svg: 'image/svg+xml',
+  ContentType.tiff: 'image/tiff',
+  ContentType.webp: 'image/webp',
+  ContentType.xhtml: 'application/xhtml+xml',
+  ContentType.xml: 'application/xml',
+  ContentType.zip: 'application/zip',
+  ContentType.js: 'text/javascript',
+  ContentType.plainText: 'text/plain',
+};
+
+const _kValueForContentType = {
+  'application/octet-stream': ContentType.binary,
+  'multipart/form-data': ContentType.formData,
+  'image/jpeg': ContentType.jpeg,
+  'application/json': ContentType.json,
+  'image/png': ContentType.png,
+  'image/avif': ContentType.avif,
+  'image/bmp': ContentType.bmp,
+  'image/gif': ContentType.gif,
+  'application/gzip': ContentType.gzip,
+  'image/vnd.microsoft.icon': ContentType.ico,
+  'application/ld+json': ContentType.jsonld,
+  'application/pdf': ContentType.pdf,
+  'image/svg+xml': ContentType.svg,
+  'image/tiff': ContentType.tiff,
+  'image/webp': ContentType.webp,
+  'application/xhtml+xml': ContentType.xhtml,
+  'application/xml': ContentType.xml,
+  'application/zip': ContentType.zip,
+  'text/javascript': ContentType.js,
+  'text/plain': ContentType.plainText,
+};
+
+const _kDefaultContentTypeEntry = MapEntry(
+  ContentType.binary,
+  'application/octet-stream',
+);
+
 enum ContentType {
+  avif,
   binary,
-  jpeg,
-  json,
-  png,
-  plainText,
+  bmp,
   formData,
+  gif,
+  gzip,
+  ico,
+  jpeg,
+  js,
+  json,
+  jsonld,
+  pdf,
+  plainText,
+  png,
+  svg,
+  tiff,
+  webp,
+  xhtml,
+  xml,
+  zip,
 }
 
 extension ContentTypeExtension on ContentType {
   String get value {
-    switch (this) {
-      case ContentType.binary:
-        return 'application/octet-stream';
-      case ContentType.formData:
-        return 'multipart/form-data';
-      case ContentType.jpeg:
-        return 'image/jpeg';
-      case ContentType.json:
-        return 'application/json';
-      case ContentType.png:
-        return 'image/png';
-      case ContentType.plainText:
-      default:
-        return 'text/plain';
-    }
+    return _kContentTypeForValue[this] ?? _kDefaultContentTypeEntry.value;
   }
 
   static ContentType of(final String value) {
-    if (value.contains('image/jpeg')) {
-      return ContentType.jpeg;
-    } else if (value.contains('application/json')) {
-      return ContentType.json;
-    } else if (value.contains('multipart/form-data')) {
-      return ContentType.formData;
-    } else if (value.contains('image/png')) {
-      return ContentType.png;
-    } else if (value.contains('text/plain')) {
-      return ContentType.plainText;
-    } else {
-      return ContentType.binary;
-    }
+    final mimeType = value.split(';').first;
+
+    return _kValueForContentType[mimeType] ?? _kDefaultContentTypeEntry.key;
   }
 }
