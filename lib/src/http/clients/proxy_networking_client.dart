37354d99ad3c1a09f3528e57c70d12e03740c297
freitzzz <1160907@isep.ipp.pt>
2022-09-28 09:57:44 +0100

    include support for system and api proxy on proxy networking client

--- lib/src/http/clients/proxy_networking_client.dart
+++ lib/src/http/clients/proxy_networking_client.dart
@@ -1,18 +1,59 @@
+import 'package:dartz/dartz.dart';
 import 'package:http/src/client.dart';
 import 'package:networking/networking.dart';
 
 class ProxyNetworkingClient extends NetworkingClient {
-  const ProxyNetworkingClient({
-    required Uri baseUrl,
-    required Client httpClient,
+  final ProxyConfiguration proxyConfiguration;
+
+  final NetworkingClient client;
+
+  ProxyNetworkingClient({
+    required this.client,
+    required this.proxyConfiguration,
   }) : super(
-          baseUrl: baseUrl,
-          httpClient: httpClient,
+          baseUrl: proxyConfiguration.uri,
+          httpClient: proxyConfiguration.client,
+          interceptors: client.interceptors,
+          timeoutDuration: client.timeoutDuration,
         );
+
+  ProxyNetworkingClient.custom({
+    required final Duration duration,
+    required final List<Interceptor> interceptors,
+    required this.client,
+    required this.proxyConfiguration,
+  }) : super(
+          baseUrl: proxyConfiguration.uri,
+          httpClient: proxyConfiguration.client,
+          timeoutDuration: duration,
+          interceptors: interceptors,
+        );
+
+  @override
+  Future<Either<RequestError, Response>> send({
+    required final Request request,
+  }) {
+    return proxyConfiguration.send?.call(request) ??
+        client.send(request: request);
+  }
 }
 
 class ProxyConfiguration {
   final Uri uri;
 
-  final 
+  final Client client;
+
+  final Future<Either<RequestError, Response>> Function(Request request)? send;
+
+  const ProxyConfiguration({
+    required this.client,
+    required this.uri,
+    this.send,
+  });
+
+  const ProxyConfiguration.api({
+    required this.client,
+    required this.uri,
+    required this.send,
+  });
 }
