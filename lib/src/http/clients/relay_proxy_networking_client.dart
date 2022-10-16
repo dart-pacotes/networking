48f8dd7b9ff3e12bd946023cab4cc3c2e0c72a3c
freitzzz <1160907@isep.ipp.pt>
2022-10-16 10:56:21 +0100

    include bypass headers injection in relay proxy configuration

--- lib/src/http/clients/relay_proxy_networking_client.dart
+++ lib/src/http/clients/relay_proxy_networking_client.dart
@@ -4,6 +4,12 @@ import 'package:networking/networking.dart';
 
 @visibleForTesting
 const kRelayDestinationServerUrlHeader = 'X-RELAY-URL';
+@visibleForTesting
+const kAlwaysIncludeBodyHeader = 'X-INCLUDE-BODY';
+@visibleForTesting
+const kBypassHeadersExposeHeader = 'X-BYPASS-EXPOSE-HEADERS';
+
+const _kDefaultValueForBypassHeaders = 'true';
 
 class RelayProxyNetworkingClient extends ProxyNetworkingClient {
   RelayProxyNetworkingClient({
@@ -11,6 +17,8 @@ class RelayProxyNetworkingClient extends ProxyNetworkingClient {
     required Uri uri,
     final Duration duration = kDefaultTimeoutDuration,
     final List<Interceptor> interceptors = kDefaultInterceptors,
+    final bool bypassBodyDelete = false,
+    final bool bypassExposeHeaders = false,
   }) : super.custom(
           duration: duration,
           interceptors: interceptors,
@@ -18,14 +26,22 @@ class RelayProxyNetworkingClient extends ProxyNetworkingClient {
           proxyConfiguration: RelayProxyConfiguration(
             client: client.httpClient,
             uri: uri,
+            bypassBodyDelete: bypassBodyDelete,
+            bypassExposeHeaders: bypassExposeHeaders,
           ),
         );
 }
 
 class RelayProxyConfiguration extends ProxyConfiguration {
+  final bool bypassBodyDelete;
+
+  final bool bypassExposeHeaders;
+
   RelayProxyConfiguration({
     required Client client,
     required Uri uri,
+    this.bypassBodyDelete = false,
+    this.bypassExposeHeaders = false,
   }) : super.api(
           client: client,
           uri: uri,
@@ -35,6 +51,10 @@ class RelayProxyConfiguration extends ProxyConfiguration {
               headers: {
                 ...request.headers,
                 kRelayDestinationServerUrlHeader: request.uri.toString(),
+                if (bypassBodyDelete)
+                  kAlwaysIncludeBodyHeader: _kDefaultValueForBypassHeaders,
+                if (bypassExposeHeaders)
+                  kBypassHeadersExposeHeader: _kDefaultValueForBypassHeaders,
               },
             );
           },
